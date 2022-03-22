e86d73ee9780a6cf7feb73077ab3c176691c13c2
hakseon-yoo <gkrtjs2020@naver.com>
2022-03-22 14:14:21 +0900

    DB환경파일수정

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connect = require('./schemas');
+require('dotenv').config();
 const app = express();
 const port = 3000;
 
--- schemas/index.js
+++ schemas/index.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
+    const mongooseId = process.env.MONGO_ID
+    const mongoosePw = process.env.MONGO_PWD
     mongoose
         //몽고디비의 기본포트 27017
-        .connect('mongodb://localhost:27017/mb_board', { ignoreUndefined: true })
+        .connect('mongodb://'+mongooseId+':'+mongoosePw+'localhost:27017/mb_board?authSource=admin&authMechanism=SCRAM-SHA-1', { ignoreUndefined: true })
         .catch((err) => {
             console.error(err)
         });
